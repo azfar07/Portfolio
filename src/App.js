bedffa8263040f7ae9752fa26e95f546868c49f9
Azfar Bin Arshad <azfar.arshad89@gmail.com>
2023-11-07 16:44:45 +0500

    update new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <Navbar />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,8 +18,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <nav className='bg-white text-black font-normal md:font-bold'> 
+        <ul className='px-2 py-4 space-x-3'>
+            <li>Benefits</li>
+            <li>Accounts</li>
+            <li>Who we are</li>
+            <li>learn</li>
+        </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
