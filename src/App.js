8d82cf30e9700c3427222b7acfdc0f3ee11a1ff3
Azfar Bin Arshad <azfar.arshad89@gmail.com>
2024-01-26 01:49:42 +0500

    register form 90 percnt complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Navbar from "./Components/Page1/Navbar/Navbar";
 import SlideShow from "./Components/Page1/SlideShow/SlideShow";
 import myImage from "./Components/assets/imageReact.png";
@@ -12,49 +14,19 @@ import Invest from "./Components/Page1/Invest/Invest";
 import Btn from "./Components/Page1/Constant/Button";
 import MainFooter from "./Components/Page1/Footer/MainFooter";
 import RegisterForm from "./Components/Page2/RegisterForm/RegisterForm";
+import MainComp from "./MainComp";
 function App() {
   return (
-    <div className="flex flex-col justify-center min-h-full pl-4 pr-4 min-w-fit">
-      <Navbar />
-      <div className="flex-grow w-full mx-auto h-max">
-        <SlideShow />
-      </div>
-      <div className="w-full mt-80">
-        <img src={myImage} alt="Image Alt Text" className="w-full" />
-      </div>
-      <div className="flex-grow w-full mx-auto mt-2 mb-32 ">
-        <CompanyInfo />
-      </div>
-      <div>
-        <FeeFree />
-      </div>
-      <div className="bg-gray-50">
-        <PaidEarly />
-      </div>
-      <div className="flex-grow w-full mx-auto mb-20 mt-36 h-max">
-        <MonthlyFees />
-      </div>
-      <div className="mb-20 bg-gray-50 mt-36">
-        <DebitCard />
-      </div>
-      <div>
-        <Invest />
-      </div>
-      <div className="flex items-center justify-center mt-1 mb-4 bg-gray-50">
-        <Btn
-          className="pt-1 mt-4 mb-4"
-          btnClassName="bg-yellow-500 text-gray-800 py-2 px-4 rounded-md shadow-md hover:shadow-lg text-xl font-bold"
-          name="Explore more campaigns"
-        />
-      </div>
-      <div>
-        <MainFooter />
-      </div>
-      <div>
-        <RegisterForm />
-      </div>
-    </div>
-  ); 
+    <Router>
+      {/* Define your routes */}
+      <Routes>
+        {/* Default route (no path specified) */}
+        <Route path="/" element={<MainComp />} />
+        {/* Route for "/register" */}
+        <Route path="/register" element={<RegisterForm />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/Components/Page1/Navbar/Navbar.js
+++ src/Components/Page1/Navbar/Navbar.js
@@ -1,36 +1,57 @@
 import React from "react";
-
+import { useNavigate,Link } from "react-router-dom";
 function Navbar() {
-
- const menuItems = [
+  const menuItems = [
     { name: "Benefits", color: "text-red-600" },
     { name: "Accounts", color: "text-green-500" },
     { name: "Who we are", color: "text-green-500" },
     { name: "Learn", color: "text-green-500" },
   ];
- 
+  const navigate = useNavigate();
+
+  const handleSignUpClick = () => {
+    // Navigate to the '/Register' route
+    navigate("/register");
+  };
   return (
     <div>
       <nav className="flex justify-between font-normal text-black bg-white md:font-bold">
-        <ul className="flex justify-start py-4 group px-28 space-x-11">
-          <li className="text-green-500 cursor-pointer">Chime</li>
-            {menuItems.map((menuItem, index) => (
-            <li
-              key={index}
-              className={`cursor-pointer text-base text-black hover:${menuItem.color} transition-colors duration-300 ease-in-out group-hover:opacity-75`}
-            >
-              {menuItem.name}
-            </li>
-            
-          ))}
-          
-        
-        </ul>
-        <ul className="flex justify-end py-1  group px-28">
-          <li className="p-3 text-base text-black transition-all duration-300 bg-transparent cursor-pointer hover:bg-green-500 hover:text-white rounded-3xl px-14">
-            Sign Up
+        <ul className="flex justify-start py-4 px-28 space-x-11">
+          <li className="text-green-500 cursor-pointer font-extrabold">
+            Chime
           </li>
-          <li className="p-3 text-base text-black transition-all duration-300 bg-transparent cursor-pointer hover:bg-green-500 hover:text-white rounded-3xl px-14">
+          <li
+            className={`cursor-pointer text-base text-black transition-colors duration-300 ease-in-out hover:text-green-500 font-bold`}
+          >
+            Benefits
+          </li>
+          <li
+            className={`cursor-pointer text-base text-black transition-colors duration-300 ease-in-out hover:text-green-500 font-bold`}
+          >
+            Accounts
+          </li>
+          <li
+            className={`cursor-pointer text-base text-black transition-colors duration-300 ease-in-out hover:text-green-500 font-bold`}
+          >
+            Who we are
+          </li>
+          <li
+            className={`cursor-pointer text-base text-black transition-colors duration-300 ease-in-out hover:text-green-500 font-bold`}
+          >
+            Learn
+          </li>
+        </ul>
+
+        <ul className="flex justify-end py-1 px-28">
+          <li
+            className="p-3 text-base text-black transition-all duration-300 bg-transparent cursor-pointer hover:bg-green-500 hover:text-white rounded-3xl px-14 font-bold"
+            onClick={handleSignUpClick}
+          >
+            <Link to="/register" className="your-styles-here">
+              Sign Up
+            </Link>
+          </li>
+          <li className="p-3 text-base text-black transition-all duration-300 bg-transparent cursor-pointer hover:bg-green-500 hover:text-white rounded-3xl px-14 font-bold">
             Login
           </li>
         </ul>
@@ -41,14 +62,14 @@ function Navbar() {
 
 export default Navbar;
 
-
- // const menuItems = [
-  //   { name: "Benefits", color: "text-red-600" },
-  //   { name: "Accounts", color: "text-green-500" },
-  //   { name: "Who we are", color: "text-green-500" },
-  //   { name: "Learn", color: "text-green-500" },
-  // ];
-  {/* {menuItems.map((menuItem, index) => (
+// const menuItems = [
+//   { name: "Benefits", color: "text-red-600" },
+//   { name: "Accounts", color: "text-green-500" },
+//   { name: "Who we are", color: "text-green-500" },
+//   { name: "Learn", color: "text-green-500" },
+// ];
+{
+  /* {menuItems.map((menuItem, index) => (
             <li
               key={index}
               className={`cursor-pointer text-base text-black hover:${menuItem.color} transition-colors duration-300 ease-in-out group-hover:opacity-75`}
@@ -57,4 +78,13 @@ export default Navbar;
             </li>
             
           ))}
-           */}
+           */
+}
+// {menuItems.map((menuItem, index) => (
+//   <li
+//     key={index}
+//     className={`cursor-pointer text-base text-black transition-colors duration-300 ease-in-out group-hover:${menuItem.color} hover:${menuItem.color} group-hover:opacity-75`}
+//   >
+//     {menuItem.name}
+//   </li>
+// ))}
